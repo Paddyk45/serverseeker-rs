806f755c63d72a49dff16018e13e68fe1e222445
enderschesi <enderschesi+github@astolfo.email>
2023-08-26 23:57:45 +0200

    Maybe this will work

--- src/models.rs
+++ src/models.rs
@@ -34,7 +34,7 @@ pub struct WhereisParams {
 }
 
 #[derive(Clone, Deserialize, Debug)]
-pub struct WhereisServers(pub Vec<WhereisServer>);
+pub struct WhereisServers(#[serde(rename(serialize = "data"))] pub Vec<WhereisServer>);
 
 impl IntoIterator for WhereisServers {
     type Item = WhereisServer;
@@ -119,7 +119,7 @@ pub struct ServersParams {
 }
 
 #[derive(Clone, Deserialize, Debug)]
-pub struct ServersServers(pub Vec<ServersServer>);
+pub struct ServersServers(#[serde(rename(serialize = "data"))] pub Vec<ServersServer>);
 
 impl IntoIterator for ServersServers {
     type Item = ServersServer;
@@ -153,8 +153,8 @@ pub struct ServersServer {
 
 // For .server_info()
 /// The server ip/port
-#[derive(Clone, Builder, Serialize)]
-#[builder(setter(into))]
+#[derive(Clone, Default, Builder, Serialize)]
+#[builder(setter(into), default)]
 pub struct ServerInfoParams {
     /// The IP of the server
     pub ip: String,
