119c3d1e4c736e05dd9a1efa04f77d40d84d26be
Patrick <patrickk0@tutanota.com>
2023-09-17 11:41:05 +0200

    Use derive_builder and many changes

--- src/lib.rs
+++ src/lib.rs
@@ -2,9 +2,16 @@
 use std::{io::Error, vec};
 
 mod models;
-pub use models::*;
+pub use models::{
+    ServerSeekerClient,
+    WhereisBuilder, WhereisBuilderError, WhereisServer,
+    ServerInfoBuilder, ServerInfoBuilderError, ServerInfoPlayer,
+    ServersBuilder, ServersBuilderError, ServerSoftware
+};
+use models::*;
 
 use serde::Deserialize;
+use derive_builder::Builder;
 
 const API_URL: &str = "https://api.serverseeker.net";
 
@@ -25,14 +32,11 @@ impl ServerSeekerClient {
 
 impl ServerSeekerClient {
     /// Get all servers a player was on during a scan
-    pub async fn whereis<F>(&self, f: F) -> Result<Vec<WhereisServer>, failure::Error> 
-    where F: FnOnce(WhereisBuilder) -> WhereisBuilder
+    pub async fn whereis(&self, builder: &WhereisBuilder) -> Result<Vec<WhereisServer>, failure::Error>
     {
         let url = format!("{API_URL}/whereis");
-        let mut builder = WhereisBuilder::new();
-        builder.params.api_key = Some(self.api_key.clone());
-        let f_builder = f(builder);
-        let params = f_builder.build();
+        let mut params = builder.build()?;
+        params.api_key = Some(self.api_key.clone());
         let body = serde_json::to_string(&params).unwrap();
         let res = self.client.post(url)
             .header("Content-Type", "application/json")
@@ -47,14 +51,11 @@ impl ServerSeekerClient {
     }
 
     /// Get a list of random servers, optionally with criteria
-    pub async fn servers<F>(&self, f: F) -> Result<Vec<ServersServer>, failure::Error>
-    where F: Fn(ServersBuilder) -> ServersBuilder
+    pub async fn servers(&self, builder: &ServersBuilder) -> Result<Vec<ServersServer>, failure::Error>
     {
         let url = format!("{API_URL}/servers");
-        let mut builder = ServersBuilder::new();
-        builder.params.api_key = Some(self.api_key.clone());
-        let f_builder = f(builder);
-        let params = f_builder.build();
+        let mut params = builder.build()?;
+        params.api_key = Some(self.api_key.clone());
         let body = serde_json::to_string(&params).unwrap();
         let res = self.client.post(url)
             .header("Content-Type", "application/json")
@@ -69,14 +70,11 @@ impl ServerSeekerClient {
     }
 
     /// Get information about a server
-    pub async fn server_info<F>(&self, f: F) -> Result<ServerInfoInfo, failure::Error>
-    where F: FnOnce(ServerInfoBuilder) -> ServerInfoBuilder
+    pub async fn server_info<F>(&self, builder: &ServersBuilder) -> Result<ServerInfoInfo, failure::Error>
     {
         let url = format!("{API_URL}/server_info");
-        let mut builder = ServerInfoBuilder::new();
-        builder.params.api_key = Some(self.api_key.clone());
-        let f_builder = f(builder);
-        let params = f_builder.build();
+        let mut params = builder.build()?;
+        params.api_key = Some(self.api_key.clone());
         let body = serde_json::to_string(&params).unwrap();
         let res = self.client.post(url)
             .header("Content-Type", "application/json")
@@ -91,130 +89,6 @@ impl ServerSeekerClient {
     }
 }
 
-impl ServersBuilder {
-    pub fn new() -> Self {
-        Self {
-            params: ServersParams {
-                api_key: None,
-                num_online_players: None, 
-                range_online_players: None,
-                max_players: None, 
-                cracked: None, 
-                description: None, 
-                protocol: None, 
-                online_after: None,
-                software: None,
-                country_code: None,
-                asn: None
-            }
-        }
-    }
-    pub fn num_online_players(mut self, value: u16) -> Self {
-        self.params.num_online_players = Some(value);
-        self
-    }
-
-    pub fn range_online_players(mut self, value: (u16, MaxOnlinePlayers)) -> Self {
-        self.params.range_online_players = Some(value);
-        self
-    }
-
-    pub fn max_players(mut self, value: u16) -> Self {
-        self.params.max_players = Some(value);
-        self
-    }
-
-    pub fn cracked(mut self, value: bool) -> Self {
-        self.params.cracked = Some(value);
-        self
-    }
-
-    pub fn description(mut self, value: String) -> Self {
-        self.params.description = Some(value);
-        self
-    }
-
-    pub fn protocol(mut self, value: i32) -> Self {
-        self.params.protocol = Some(value);
-        self
-    }
-
-    pub fn online_after(mut self, value: u16) -> Self {
-        self.params.online_after = Some(value);
-        self
-    }
-
-    pub fn software(mut self, value: ServerSoftware) -> Self {
-        self.params.software = Some(value);
-        self
-    }
-
-    pub fn country_code(mut self, value: String) -> Self {
-        self.params.country_code = Some(value);
-        self
-    }
-
-    pub fn asn(mut self, value: i16) -> Self {
-        self.params.asn = Some(value);
-        self
-    }
-
-    fn build(self) -> ServersParams {
-        self.params
-    }
-}
-
-impl WhereisBuilder {
-    pub fn new() -> Self {
-        Self {
-            params: WhereisParams {
-                api_key: None, 
-                name: None, 
-                uuid: None
-            }
-        }
-    }
-
-    pub fn name(mut self, value: String) -> Self{
-        self.params.name = Some(value);
-        self
-    }
-
-    pub fn uuid(mut self, value: String) -> Self{
-        self.params.uuid = Some(value);
-        self
-    }
-
-    fn build(self) -> WhereisParams {
-        self.params
-    }
-}
-
-impl ServerInfoBuilder {
-    pub fn new() -> Self {
-        Self {
-            params: ServerInfoParams {
-                api_key: None, 
-                ip: String::new(), 
-                port: None
-            }
-        }
-    }
-
-    pub fn ip(mut self, value: String) -> Self{
-        self.params.ip = value;
-        self
-    }
-
-    pub fn port(mut self, value: u16) -> Self{
-        self.params.port = Some(value);
-        self
-    }
-
-    fn build(self) -> ServerInfoParams {
-        self.params
-    }
-}
 
 //      |\      _,,,---,,_
 //ZZZzz /,`.-'`'    -.  ;-;;,_
--- src/models.rs
+++ src/models.rs
@@ -1,3 +1,4 @@
+use derive_builder::Builder;
 use serde::{Deserialize, Serialize, Serializer};
 
 /// A ServerSeeker client which stores the api key
@@ -8,25 +9,23 @@ pub struct ServerSeekerClient {
 
 /// An error
 #[derive(Deserialize, Debug)]
-pub struct APIError {
+pub(crate) struct APIError {
     pub error: String
 }
 
 // For .whereis():
-#[doc(hidden)]
-#[derive(Serialize)]
-pub struct WhereisParams {
+#[derive(Serialize, Builder, Default)]
+#[builder(name = "WhereisBuilder", public, setter(strip_option), default)]
+pub(crate) struct WhereisParams {
     /// Your api_key
+    #[builder(setter(skip))]
     pub api_key: Option<String>,
+
     /// The name of the player you want to find
     pub name: Option<String>,
+
     /// The uuid of the player you want to find
     pub uuid: Option<String>
-}
-
-#[doc(hidden)]
-pub struct WhereisBuilder {
-    pub params: WhereisParams
 }
 
 /// A server in the results
@@ -34,23 +33,27 @@ pub struct WhereisBuilder {
 pub struct WhereisServer {
     /// The last time the player was seen on the server (unix timestamp)
     pub last_seen: i64,
+
     /// The name of the player
     pub name: String,
+
     /// The ip:port of the server
     pub server: String,
+
     /// The uuid of the player 
     pub uuid: String
 }
 
 /// The data array from the response
 #[derive(Deserialize, Debug)]
-pub struct WhereisData {
+pub(crate) struct WhereisData {
     /// An array of servers the player was seen on. Limited to 1000
     pub data: Vec<WhereisServer>
 }
 
 
 // For .servers():
+#[derive(Clone)]
 pub enum MaxOnlinePlayers {
     Num(u16),
     Inf
@@ -71,7 +74,7 @@ impl Serialize for MaxOnlinePlayers {
 }
 
 /// Software of a server
-#[derive(Serialize)]
+#[derive(Serialize, Clone)]
 pub enum ServerSoftware {
     Any,
     Vanilla,
@@ -81,40 +84,47 @@ pub enum ServerSoftware {
 }
 
 /// The search parameters
-#[derive(Serialize)]
-pub struct ServersParams {
+#[derive(Serialize, Builder, Clone, Default)]
+#[builder(name = "ServersBuilder", public, setter(strip_option), default)]
+pub(crate) struct ServersParams {
     /// Your api_key
-    pub api_key: Option<String>,
+    #[builder(setter(skip))]
+    pub(crate) api_key: Option<String>,
+
     /// The amount of online players the server should have
     #[serde(rename = "online_players")]
     #[serde(skip_serializing_if = "Option::is_none")]
     pub num_online_players: Option<u16>,
+
     /// Min-Max range of online players
     #[serde(rename = "online_players")]
     #[serde(skip_serializing_if = "Option::is_none")]
     pub range_online_players: Option<(u16, MaxOnlinePlayers)>,
+
     /// The maximum amount of players the server should hold
     pub max_players: Option<u16>,
+
     /// Whether the server should be cracked or not (onlime mode disabled)
     pub cracked: Option<bool>,
+
     /// What the description aka MOTD of the servers should contain
     pub description: Option<String>,
+
     /// The [protocol version](https://wiki.vg/Protocol_version_numbers) of the server
     pub protocol: Option<i32>,
+
     /// The server should have been online after this unix timestamp Defaults to showing all servers which were online at last ping.
     pub online_after: Option<u16>,
+
     /// The software of the server
     #[serde(skip_serializing_if = "Option::is_none")]
     pub software: Option<ServerSoftware>,
+
     /// The country code of the server. See [here](https://en.wikipedia.org/wiki/ISO_3166-1_alpha-2)
     pub country_code: Option<String>,
+
     /// The AS number of the server. You can get it easily from ipinfo. See [here](https://en.wikipedia.org/wiki/Autonomous_system_(Internet))
     pub asn: Option<i16>
-}
-
-#[doc(hidden)]
-pub struct ServersBuilder {
-    pub params: ServersParams
 }
 
 /// A server in the results
@@ -122,44 +132,50 @@ pub struct ServersBuilder {
 pub struct ServersServer {
     /// Whether the server is cracked or not. None if unknown
     pub cracked: Option<bool>,
+
     /// The MOTD of the server
     pub description: String,
+
     /// The last time the server was seen (unix timestamp)
     pub last_seen: i64,
+
     /// The maximum amount of players the server can hold
     pub max_players: u32,
+
     /// The amount of players online during the last scan
     pub online_players: u32,
+
     /// The [protocol version](https://wiki.vg/Protocol_version_numbers) of the server
     pub protocol: i64,
+
     /// The ip:port of the server
     pub server: String,
+
     /// The minecraft version of the server
     pub version: String,
 }
 
 /// The data array from the response
 #[derive(Deserialize, Debug)]
-pub struct ServersData {
+pub(crate) struct ServersData {
     pub data: Vec<ServersServer>
 }
 
 
 // For .server_info()
 /// The server ip/port
-#[derive(Serialize)]
+#[derive(Serialize, Builder, Default)]
+#[builder(name = "ServerInfoBuilder", public, setter(strip_option), default)]
 pub struct ServerInfoParams {
     /// Your api_key
+    #[builder(setter(skip))]
     pub api_key: Option<String>,
+
     /// The IP of the server
     pub ip: String,
+
     /// The port of the server (default=25565)
     pub port: Option<u16>
-}
-
-#[doc(hidden)]
-pub struct ServerInfoBuilder {
-    pub params: ServerInfoParams
 }
 
 /// The information about the server
@@ -167,18 +183,25 @@ pub struct ServerInfoBuilder {
 pub struct ServerInfoInfo {
     /// Whether the server is cracked or not. None if unknown
     pub cracked: Option<bool>,
+
     /// The description (MOTD) of the server
     pub description: String,
+
     /// The last time the server was seen (unix timestamp)
     pub last_seen: i64,
+
     /// The maximum amount of players the server can hold
     pub max_players: u32,
+
     /// The amount of players online during the last scan
     pub online_players: u16,
+
     /// The [protocol version](https://wiki.vg/Protocol_version_numbers) of the server
     pub protocol: i64,
+
     /// The minecraft version of the server
     pub version: String,
+
     /// An array of when which players were seen on the server. Limited to 1000
     pub players: Vec<ServerInfoPlayer>
 }
@@ -188,8 +211,10 @@ pub struct ServerInfoInfo {
 pub struct ServerInfoPlayer {
     /// The last time the player was seen on the server (unix timestamp)
     pub last_seen: i64,
+
     /// The name of the player
     pub name: String,
+
     /// The uuid of the player
     pub uuid: String
 }
